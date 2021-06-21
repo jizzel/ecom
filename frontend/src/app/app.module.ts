b3b44216b0eec670bc7bd052bcd1b07fa76d3b3d
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2021-06-21 21:28:57 +0300

    refactored modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,35 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {CarouselComponent} from './shared/components/carousel/carousel.component';
 import { HomeComponent } from './core/home/home.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { LoginComponent } from './core/login/login.component';
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
-import { CardComponent } from './shared/components/card/card.component';
-import { FlatButtonComponent } from './shared/components/flat-button/flat-button.component';
-import { RaisedButtonComponent } from './shared/components/raised-button/raised-button.component';
-
-
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarouselComponent,
     HomeComponent,
     NavbarComponent,
     FooterComponent,
     LoginComponent,
-    PageNotFoundComponent,
-    CardComponent,
-    FlatButtonComponent,
-    RaisedButtonComponent
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,28 +1,27 @@
-import { Component, OnInit,AfterViewInit } from '@angular/core';
-import * as M from 'materialize-css';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {CardComponent} from "./components/card/card.component";
+import {CarouselComponent} from "./components/carousel/carousel.component";
+import {FlatButtonComponent} from "./components/flat-button/flat-button.component";
+import {RaisedButtonComponent} from "./components/raised-button/raised-button.component";
 
-@Component({
-  selector: 'app-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+
+
+@NgModule({
+  declarations: [
+    CardComponent,
+    CarouselComponent,
+    FlatButtonComponent,
+    RaisedButtonComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CardComponent,
+    CarouselComponent,
+    FlatButtonComponent,
+    RaisedButtonComponent
+  ]
 })
-export class CarouselComponent implements OnInit,AfterViewInit {
-  options= {fullWidth:true,indicators: true, duration: 200}
-  items=["../../assets/images/img1.jpg", 
-        "../../assets/images/img1.jpg", 
-        "../../assets/images/img1.jpg",
-        "../../assets/images/img1.jpg"]
-
-  hrefs=['one', 'two', 'three', 'four','five'];
-        
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  ngAfterViewInit(){
-    const elems = document.querySelectorAll('.carousel');
-    M.Carousel.init(elems,this.options);
-  }
-}
-
+export class SharedModule { }
