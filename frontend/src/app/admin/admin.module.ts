18c8f56c11d86e25730a5f026e6c6bb9c22d3578
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2021-06-21 20:54:33 +0300

    added product and products components

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import {AdminOrdersComponent} from "./components/admin-orders/admin-orders.component";
-import { AdminComponent } from './admin/admin.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductComponent } from './components/product/product.component';
+import {AdminComponent} from "./admin.component";
 
 
 @NgModule({
   declarations: [
     AdminOrdersComponent,
-    AdminComponent
+    AdminComponent,
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     CommonModule,
