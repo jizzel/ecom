c2fb8d36469109fd33315a85d685f52c266a3fa0
Jessie Effah <jekwakyewa@gmail.com>
2021-08-06 01:14:33 +0000

    logic created for increasing or decreasing product items in shopping cart component

--- frontend/src/app/shopping/shopping-cart/shopping-cart.component.ts
+++ frontend/src/app/shopping/shopping-cart/shopping-cart.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { RaisedButtonComponent  } from '../../shared/components/raised-button/raised-button.component';
-
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -10,14 +10,19 @@ import { RaisedButtonComponent  } from '../../shared/components/raised-button/ra
 })
 
 export class ShoppingCartComponent implements OnInit {
+  //NOTE: Without uncommenting the strict:true in the tsconfig file the viewchild would throw an error
+  @ViewChild('inputNumber', {static: false}) inputNumber: ElementRef;
+
   tableHeaderNames=['Item', 'Quantity', 'Price'];
   buttonName:string = "Checkout";
-  numberOfItemsInCart:number=0;
-  
+  numberOfItemsInCart:number= 0;
+  currentItemNumber:number;
 
-  constructor(private route:Router) { }
+  constructor(private route:Router) {
+   }
 
   ngOnInit(): void {
+    
   }
 
   onClickCheckOut(){
@@ -42,9 +47,31 @@ export class ShoppingCartComponent implements OnInit {
     },
   ]
 
+  reduceItemNumber(){
+    this.currentItemNumber = parseInt(this.inputNumber.nativeElement.value);
+     //if currentItemNumber is a valid number  and if currentItemNumber is greater than 0, subtract one to it otherwise make it 0
+    if(!isNaN(this.currentItemNumber)  && this.currentItemNumber > 0){
+      this.inputNumber.nativeElement.value= this.currentItemNumber - 1;
+    }else{
+      this.inputNumber.nativeElement.value = "0";
+    }
+  }
+
+  increaseItemNumber(){
+    this.currentItemNumber = parseInt(this.inputNumber.nativeElement.value);
+    //if currentValue is a valid number add one to it otherwise make it 0
+    if(!isNaN(this.currentItemNumber)){
+      this.inputNumber.nativeElement.value= this.currentItemNumber + 1;
+    }else{
+      this.inputNumber.nativeElement.value = "0";
+    }
+  }
+  
   deleteShoppingCart(){
     this.shoppingCartItems=[];
-  }
+  } 
 }
+
+
 
 
